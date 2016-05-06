8510f180af67ed3f689bc10bbbd858ca9c744611
Jaren831 <jaren.lynchmobb@gmail.com>
2016-05-06 00:49:44 -0700

    Wiki button and search icon working

--- wiki.js
+++ wiki.js
@@ -1,4 +1,29 @@
-document.getElementById("wiki").onclick = function() {
+function wikiRandom() {
 	window.open("https://en.wikipedia.org/wiki/Special:Random");
 };
 
+function wikiSearch(entry) {
+	window.open("https://en.wikipedia.org/wiki/" + entry);
+};
+
+document.getElementById("wiki").onclick = function() {
+	wikiRandom();
+};
+
+document.getElementById("searchBtn").onclick = function() {
+	wikiSearch(document.getElementById("searchEntry").value);
+};
+
+$(document).ready(function(){
+
+    $(document).bind('keypress',pressed);
+});
+
+function pressed(e)
+{
+    if(e.keyCode === 13)
+    {
+        alert('enter pressed');
+        //put button.click() here
+    }
+}
